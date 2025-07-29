e0bb5ab99172aea1428448d80698a9f5adf70c15
CleeYOpro <cbalaranjith@gmail.com>
2025-07-28 18:14:27 -0700

    change color of hero title

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -49,7 +49,7 @@ const HeroContent = styled.div`
 const Title = styled.h1`
   font-size: 2rem;
   margin-bottom: 1rem;
-  color: #fff;
+  color: var(--primary-color);
 `;
 
 const Description = styled.p`
